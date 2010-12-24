121e882aa87a1959c7f741194f5f241ab1b64caa
AJ ONeal <coolaj86@gmail.com>
2010-12-24 06:13:57 -0700

    updated socket test with more output

--- tests/unix-stream-server-test.js
+++ tests/unix-stream-server-test.js
@@ -2,26 +2,33 @@
 (function () {
   "use strict";
   
-  // TODO cliest should handlee ECONREF
-  var net = require('net');
+  // TODO client should handle ECONREF
+  var net = require('net'),
+    util = require('util');
 
   net.createServer(function (stream) {
+
     stream.setEncoding('binary');
     stream.setTimeout(256);
+
     stream.on('connect', function () {
+      util.print("(o)");
       //stream.write('hello\r\n');
     });
     stream.on('data', function (data) {
+      util.print("(d)");
       //stream.write(data);
     });
     stream.on('end', function () {
-      console.log("client ended");
+      util.print("(e)");
       //stream.write('goodbye\r\n');
       //stream.end(); // leak the connection
     });
+    stream.on('close', function () {
+      util.print("(c)\n");
+    });
     stream.on('timeout', function () {
-      //console.log("not closing, allowing to leak");
-      console.log("client went inactive");
+      util.print("(t)");
       stream.end();
     });
   }).listen("/tmp/node-unix-stream-server-test.sock");
