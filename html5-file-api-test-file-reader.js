0283bd8a5b89f4eed76b4a9d6bd78b6d89637ba7
AJ ONeal <coolaj86@gmail.com>
2011-01-11 14:28:52 -0700

    completed html5-file-api

--- html5-file-api-test-file-reader.js
+++ html5-file-api-test-file-reader.js
@@ -13,107 +13,185 @@
     },
     fileStat = fs.statSync(fileData.path),
     lastModifiedDate = fileStat.mtime.toISOString(),
-    file;
-
+    fileBuffer = fs.readFileSync(fileData.path),
+    fileBase64 = fileBuffer.toString('base64'),
+    fileDataUrl = "data:image/jpeg;base64,",
+    file,
+    chunkedEncoding = (new Date().valueOf() % 2) ? true : false;
 
 
 
   sequence
 
+    //
+    // Test DataURL conversion with different file load methods
+    //
 
-    // By file name with options
+    // Give Path
+    // Give both onload and addEventListener
     .then(function (next) {
-      var fileReader = new FileReader(new File(fileData));
-      console.log(fileReader);
-      //assert.deepEqual(file, fileData);
-      
-      //next();
-    })
+      var fileReader = new FileReader(),
+        onloaded,
+        size = 0,
+        count = 0;
 
 
-    // By file name with sync data for size and lastModified
-    .then(function (next) {
-      fileData.jsdom = true; 
-      file = new File(fileData);
-      delete file.stat;
-      file.lastModifiedDate = file.lastModifiedDate.toISOString();
-      assert.deepEqual(file, { path: './files/coolaj86-2010.jpg'
-        , name: 'coolaj86-2010.jpg'
-        , type: 'image/jpeg'
-        , jsdom: true
-        , lastModifiedDate: lastModifiedDate
-        , size: fileStat.size
+      // non-standard, node-only `data`
+      fileReader.addEventListener('data', function (data) {
+        count += 1;
+        size += data.length;
+        //console.log("no-chunk:", data.length);
       });
-      next();
-    })
 
 
-    // By file name with async data for stat
-    .then(function (next) {
-      fileData.async = true; 
-      file = new File(fileData);
-      assert.deepEqual(file, { path: './files/coolaj86-2010.jpg'
-        , name: 'coolaj86-2010.jpg'
-        , type: 'image/jpeg'
-        , jsdom: true
-        , async: true
+      // TODO Futures.join
+      fileReader.addEventListener('load', function (ev) {
+        onloaded = true;
+        assert.strictEqual(ev.target.result.length, fileDataUrl.length + fileBase64.length);
       });
-      setTimeout(function () {
-        delete file.stat;
-        file.lastModifiedDate = file.lastModifiedDate.toISOString();
-        assert.deepEqual(file, { path: './files/coolaj86-2010.jpg'
-          , name: 'coolaj86-2010.jpg'
-          , type: 'image/jpeg'
-          , jsdom: true
-          , async: true
-          , lastModifiedDate: lastModifiedDate
-          , size: fileStat.size
-        });
+      fileReader.onload = function (ev) {
+        assert.strictEqual(count, 1);
+        assert.ok(onloaded);
+        assert.strictEqual(ev.target.result.length, fileDataUrl.length + fileBase64.length);
         next();
-      }, 100);
-    })
-
-
-    // Using file buffer
-    .then(function (next) {
-      fs.readFile(fileData.path, function (err, data) {
-        var fileBuffer = {
-          name: "coolaj86-2010.jpg",
-          buffer: data
-        };
-
-        file = new File(fileBuffer);
-        delete file.buffer;
-        assert.deepEqual(file, { name: fileData.name
-          , type: 'image/jpeg'
-          , size: fileStat.size
-        });
-        next();
-      });
-    })
-
-
-    // Using file stream
-    .then(function (next) {
-      var fileStream = {
-        name: fileData.name,
-        stream: fs.createReadStream(fileData.path)
       };
 
-      file = new File(fileStream);
-      delete file.stream;
-      assert.deepEqual(file, { name: fileData.name
-        , type: 'image/jpeg'
-      });
-      next();
+      fileReader.setNodeChunkedEncoding(false);
+      fileReader.readAsDataURL(new File(fileData));
     })
 
 
-    // Passes!
+    // Give Path
+    // Test chunked encoding
     .then(function (next) {
-      console.log("all tests pass");
-    });
+      var fileReader = new FileReader(),
+        size = 0,
+        count = 0;
 
+      
+      // non-standard, node-only `data`
+      fileReader.addEventListener('data', function (data) {
+        count += 1;
+        size += data.length;
+        //console.log("chunk:", data.length);
+      });
+
+
+      fileReader.addEventListener('load', function (ev) {
+        assert.ok(count > 1);
+        assert.strictEqual(ev.target.result.length, fileDataUrl.length + fileBase64.length);
+        assert.strictEqual(ev.target.nodeBufferResult.length, fileBuffer.length);
+        assert.strictEqual(size, fileBuffer.length);
+        next();
+      });
+
+
+      fileReader.setNodeChunkedEncoding(true);
+      fileReader.readAsDataURL(new File(fileData));
+    })
+
+
+    // Give Buffer
+    .then(function (next) {
+      var fileReader = new FileReader();
+
+      fileReader.addEventListener('load', function (ev) {
+        assert.strictEqual(ev.target.result.length, fileDataUrl.length + fileBase64.length);
+        fileData.buffer = undefined;
+        delete fileData.buffer;
+        next();
+      });
+
+      fileData.buffer = fileBuffer;
+      // will not matter when the data is a buffer
+      fileReader.setNodeChunkedEncoding(chunkedEncoding);
+      fileReader.readAsDataURL(new File(fileData));
+    })
+
+
+    // Give Stream
+    .then(function (next) {
+      var fileReader = new FileReader(),
+        fileStream = fs.createReadStream(fileData.path),
+        size = 0;
+
+      // non-standard, node-only
+      fileReader.addEventListener('data', function (data) {
+        size += data.length;
+      });
+
+      fileReader.addEventListener('load', function (ev) {
+        assert.strictEqual(ev.target.result.length, fileDataUrl.length + fileBase64.length);
+        assert.strictEqual(ev.target.nodeBufferResult.length, fileBuffer.length);
+        assert.strictEqual(size, fileBuffer.length);
+        fileData.stream = undefined;
+        delete fileData.stream;
+        next();
+      });
+
+      fileData.stream = fileStream;
+      // will not matter when the data is a stream
+      fileReader.setNodeChunkedEncoding(chunkedEncoding);
+      fileReader.readAsDataURL(new File(fileData));
+    })
+
+
+
+    //
+    // Test readAsXyz methods
+    //
+
+    // ArrayBuffer
+    .then(function (next) {
+      var fileReader = new FileReader();
+
+      fileReader.addEventListener('load', function (ev) {
+        assert.strictEqual(ev.target.result.length, fileBuffer.length);
+        next();
+      });
+
+      fileData.buffer = fileBuffer;
+      fileReader.readAsArrayBuffer(new File(fileData));
+    })
+
+
+    // BinaryString
+    .then(function (next) {
+      var fileReader = new FileReader();
+
+      fileReader.addEventListener('load', function (ev) {
+        assert.strictEqual(ev.target.result.length, fileBuffer.toString('binary').length);
+        next();
+      });
+
+      fileData.buffer = fileBuffer;
+      fileReader.readAsBinaryString(new File(fileData));
+    })
+
+
+    // Text
+    .then(function (next) {
+      var fileReader = new FileReader();
+
+      fileReader.addEventListener('load', function (ev) {
+        assert.strictEqual(ev.target.result.length, fileBuffer.toString('utf8').length);
+        assert.strictEqual(ev.target.result.length, fileBuffer.toString('ascii').length);
+        next();
+      });
+
+      fileData.buffer = fileBuffer;
+      fileReader.readAsText(new File(fileData));
+    })
+
+
+    // DataURL well-tested in first section
+
+
+    // Pass
+    .then(function (next) {
+      console.log("No test failed assertions. Reached final test. All tests (ostensibly) passed.");
+      next();
+    });
 
   if ('undefined' === typeof provide) { provide = function() {}; }
   provide('html5-file-api-test');
