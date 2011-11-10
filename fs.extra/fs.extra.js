ea7aa202d85a9ef06e3019bb557f63babd7fcf41
AJ ONeal <coolaj86@gmail.com>
2011-11-10 00:16:01 -0700

    added fs.move

---
+++ fs.extra/fs.extra.js
@@ -0,0 +1,54 @@
+(function () {
+  "use strict";
+
+  var fs = require('fs')
+    , util = require('util')
+    ;
+
+  fs.copy = function (src, dst, cb) {
+    function copy(err) {
+      var is
+        , os
+        ;
+
+      if (!err) {
+        return cb(new Error("File " + dst + " exists."));
+      }
+
+      fs.stat(src, function (err) {
+        if (err) {
+          return cb(err);
+        }
+        is = fs.createReadStream(src);
+        os = fs.createWriteStream(dst);
+        util.pump(is, os, cb);
+      });
+    }
+
+    fs.stat(dst, copy);
+  };
+
+  fs.move = function (src, dst, cb) {
+    function copyIfFailed(err) {
+      if (!err) {
+        return cb(null);
+      }
+      fs.copy(src, dst, function(err) {
+        if (!err) {
+          // TODO 
+          // should we revert the copy if the unlink fails?
+          fs.unlink(src, cb);
+        } else {
+          cb(err);
+        }
+      });
+    }
+
+    fs.stat(dst, function (err) {
+      if (!err) {
+        return cb(new Error("File " + dst + " exists."));
+      }
+      fs.rename(src, dst, copyIfFailed);
+    });
+  };
+}());
