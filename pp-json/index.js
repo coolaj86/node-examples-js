56cee579e1e6302a949f765d008dd2875f5ede06
AJ ONeal <coolaj86@gmail.com>
2012-01-04 16:02:38 -0700

    added ability to read from stdin, clarified error messages

--- pp-json/index.js
+++ pp-json/index.js
@@ -8,13 +8,58 @@
     , json
     ;
 
-  try {
-    json = JSON.parse(fs.readFileSync(filename));
-  } catch(e) {
-    console.error("[ERROR]");
-    throw e;
+  function ppJson() {
+    json = prettyPrint(json, null, '  ');
+    console.log(json);
   }
 
-  json = prettyPrint(json, null, '  ');
-  console.log(json);
+  function readJsonFile() {
+    try {
+      json = JSON.parse(fs.readFileSync(filename));
+    } catch(e) {
+      console.error("[ERROR] couldn't parse JSON from '" + filename + "':");
+      console.error(e.message);
+    }
+
+    ppJson();
+  }
+
+  function readJsonStdin() {
+    var timeoutToken
+      , stdin = process.openStdin()
+      ;
+
+    timeoutToken = setTimeout(function () {
+      console.warn("Usage: pp-json /path/to/data.json");
+      console.warn("Usage: echo '{ \"foo\": \"bar\" }' | pp-json");
+      process.exit();
+    }, 1000);
+
+    json = '';
+
+    stdin.on('data', function (chunk) {
+      clearTimeout(timeoutToken);
+      json += chunk;
+    });
+    
+    stdin.on('end', function () {
+      try {
+        json = JSON.parse(json);
+      } catch(e) {
+        console.error("[ERROR] couldn't parse JSON from stdin:");
+        console.error(e.message);
+        return;
+      }
+
+      ppJson();
+    });
+  }
+
+  if (filename) {
+    readJsonFile();
+  }
+  else {
+    readJsonStdin();
+  }
+
 }());
